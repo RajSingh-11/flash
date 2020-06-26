340a1b4901295fb91df20ad8479ac022e7f5efca
rajender.singh <rajender.singh@zillious-12.ZILLIOUS>
2020-06-26 10:37:14 +0530

    Test2 files committed to local repository

---
+++ Test2/src/com/singh/Test2.java
@@ -0,0 +1,19 @@
+package com.singh;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("test");
+		add();
+	}
+
+	private static void add() {
+		// TODO Auto-generated method stub
+		int a = 12, b = 21, c;
+
+		c = a + b;
+		System.out.println(c);
+	}
+}
